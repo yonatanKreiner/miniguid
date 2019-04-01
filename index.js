3bfe48ea59d611a77f8ab36dc65b4aeafcb82f65
yonatan kreiner <yonatankreiner@gmail.com>
2019-04-01 23:20:08 +0300

    update dependencies

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 'use strict';
 
 var base64url = require('base64url');
-var guid = require('guid');
+var uuidv4 = require('uuid/v4');
 
 /**
  * Returns a 22 chars string GUID
  * @return {string}
  */
 module.exports = function() {
-    var rawGuid = guid.raw();
+    var rawGuid = uuidv4();
     var guidBytesArray = guidToBytes(rawGuid);
     return base64url.encode(guidBytesArray);
 };
 
-function guidToBytes(guid) {
+function guidToBytes(uuid) {
     var bytes = [];
-    guid.split('-').map((number, index) => {
+    uuid.split('-').map((number, index) => {
         var bytesInChar = index < 3 ? number.match(/.{1,2}/g).reverse() :  number.match(/.{1,2}/g);
         bytesInChar.map((byte) => { bytes.push(parseInt(byte, 16));})
     });
